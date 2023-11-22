29db720b4a53d0c8b4083abc75b966e41e02e79a
iwahashi0910 <i.yuya0910@gmail.com>
2023-11-22 22:51:30 +0900

    Homeにリンク追加

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,42 @@
 <template>
   <h1>Home</h1>
-  <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
+  <nav class="navigation1">
+    <router-link
+      to="/quiz"
+      class="navigation__link1"
+      exact-active-class="navigation__link--exact-active1"
+      >Vue クイズ</router-link
+    >/
+    <router-link
+      to="/palette"
+      class="navigation__link1"
+      exact-active-class="navigation__link--exact-active1"
+      >Vue パレット</router-link
+    >/
+    <router-link
+      to="/memo"
+      class="navigation__link1"
+      exact-active-class="navigation__link--exact-active1"
+      >Vue メモ</router-link
+    >
+  </nav>
 </template>
+
+<style>
+.navigation1 {
+  display: flex;
+  font-style: italic;
+  align-items: flex-end;
+  gap: 0.8rem;
+  font-size: 1.5rem;
+}
+
+.navigation__link1 {
+  font-weight: bold;
+  color: blue;
+}
+
+.navigation__link--exact-active1 {
+  color: #42b983;
+}
+</style>
