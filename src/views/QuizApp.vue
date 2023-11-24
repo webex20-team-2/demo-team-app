6fe48e289f351ec6725933a181f1db8734c774d3
hamuhamu5hamu <hamuh5266@gmail.com>
2023-11-24 21:01:44 +0900

    クイズ作成をしました。
    フッターに作成者の名前を入れました。

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -1,29 +1,46 @@
 <template>
   <h1>Vue クイズ</h1>
   <div class="app">
-    <h2>Q. {{ "クイズタイトル" }}</h2>
-    <img
-      class="quiz-image"
-      src="https://via.placeholder.com/300x300"
-      alt="クイズタイトル"
-    />
-    <div class="container">
-      <button>
-        {{ "選択肢1" }}
+    <h2>Q. {{ "この星の名前は何でしょう？" }}</h2>
+    <img class="quiz-image" src="../assets/Ganymede.jpg" alt="クイズタイトル" />
+    <div id="container">
+      <button v-on:click="cho1()">
+        {{ choi1 }}
       </button>
-      <button>
-        {{ "選択肢2" }}
+      <button v-on:click="cho2()">
+        {{ choi2 }}
       </button>
-      <button>
-        {{ "選択肢3" }}
+      <button v-on:click="cho3()">
+        {{ choi3 }}
       </button>
     </div>
-    <div>{{ "答え" }}</div>
+    <div>{{ ans }}</div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      choi1: "ゴリアテ",
+      choi2: "ゼニガメ",
+      choi3: "ガニメデ",
+      ans: "答え",
+    }
+  },
+  methods: {
+    cho1() {
+      this.ans =
+        "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。"
+    },
+    cho2() {
+      this.ans = "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。"
+    },
+    cho3() {
+      this.ans = "正解！ガニメデは、木星の第三惑星だよ！"
+    },
+  },
+}
 </script>
 
 <style>
